177f263d450573a5423f7c5250db1370733c9645
unknown <seppe.bellens@hotmail.nl>
2022-12-15 14:12:31 +0100

    Fix[main.js]: fix bug with menu while resizing

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ const hamburgerMenu = document.querySelector(".hamburger");
 const dropdownItems = document.querySelector(".dropdownItems");
 const body = document.querySelector("body");
 
+
+window.addEventListener("resize",function(e){
+    if(document.body.offsetWidth >= "992"){
+        dropdownItems.setAttribute("style","display: none;");
+    }
+});
+
 hamburgerMenu.addEventListener("click",function(e){
-    console.log(dropdownItems.getAttribute("style"));
     if( dropdownItems.getAttribute("style") === "display: none;"){
-        console.log("kaas");
         dropdownItems.setAttribute("style","display: flex;");
     } else if(dropdownItems.getAttribute("style") === "display: flex;")  {
-        console.log("geen kaas");
         dropdownItems.setAttribute("style","display: none;");
     }
 });
